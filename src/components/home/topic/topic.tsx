21295ec1228c56783c199283330bf8bf06978cdd
Lương Trần <luongtg@lumi.biz>
2022-08-07 11:17:23 +0700

    fix: change topic content

--- src/components/home/topic/topic.tsx
+++ src/components/home/topic/topic.tsx
@@ -10,12 +10,13 @@ type TopicProps = {
 
 const Topic = ({link, image, header, meta}: TopicProps): JSX.Element => {
   return (
-    <div className={styles.root}>
-      <h1>{link}</h1>
-      <h1>{image}</h1>
-      <h1>{header}</h1>
-      <h1>{meta}</h1>
-    </div>
+    <>
+      <div className={styles.root}>
+        <h2>{header}</h2>
+        <p><i>{meta}</i></p>
+      </div>
+      <div className={styles.line}/>
+    </>
   )
 }
 
