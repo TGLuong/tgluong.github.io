39c1a8e1079fd9335425373a31ce76e0cbc38e6a
Lương Trần <luongtg@lumi.biz>
2022-08-21 17:59:46 +0700

    feat: bookmark component

--- src/components/common/bookmark.tsx
+++ src/components/common/bookmark.tsx
@@ -1,23 +1,37 @@
 import styles from 'styles/scss/common/bookmark.module.scss'
+import {BsFillBookmarkStarFill} from 'react-icons/bs'
+import cn from 'classnames'
+import { Icon } from '@chakra-ui/react'
 
+export type BookMarkElement = {
+  datas: Array<{
+    id: any,
+    title: string,
+    component: JSX.Element
+  }>,
+  onSelect: any,
+  selected: number
+}
 
-const datas = [
-  "fsdjfksdfkdkf",
-  "fjdksfjdksfjdslkfjdkjf",
-  "fjdksjfkdsjd",
-  "fjdskfjkds",
-  "fjdkjsdkfjdkfskdjfkjd"
-]
-
-const BookMark = () => {
+const BookMark = ({datas, onSelect, selected}: BookMarkElement) => {
+  console.log(`selected: ${selected}`)
   return (
-    <>
-      <div className={styles.root}>
-        <h2>Contents</h2>
-        <p>Lorem ipsum dolor sit amet.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-      </div>
-    </>
+    <div className={styles.root}>
+      <h2>Contents</h2>
+      {datas.map((element) => {
+        return (
+          <p 
+            key={element.id}
+            onClick={() => { 
+              onSelect(element.id, element.component)
+            }}
+            className={cn({[styles['selected']]: selected == element.id})}
+          >
+            <Icon as={BsFillBookmarkStarFill} />{element.title}
+          </p>
+        )
+      })}
+    </div>
   )
 }
 
