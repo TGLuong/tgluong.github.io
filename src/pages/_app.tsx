9141623add2d53efeb45b7f5f8ab61bef3c4d18e
Lương Trần <luongtg@lumi.biz>
2022-09-07 23:38:30 +0700

    fix: delete Head

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,6 @@ import { ChakraProvider } from '@chakra-ui/react'
 import 'styles/globals.css'
 import styles from 'styles/scss/main.module.scss'
 import Layout from '../components/layout'
-import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
